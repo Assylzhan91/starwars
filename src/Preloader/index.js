6bb7c44ebb3d968becc41a339c2ffd0d92df5422
Assylzhan91 <asylzhan.baimuratov@gmail.com>
2020-04-11 02:00:17 +0600

    Commit-01

---
+++ src/Preloader/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./classes.css"
+import "./classes.css"
+
+export default  props => {
+    return (
+        <div className="lds-spinner">
+            <div/><div/>
+            <div/><div/>
+            <div/><div/>
+            <div/><div/>
+            <div/><div/>
+            <div/><div/>
+        </div>
+    )
+}
