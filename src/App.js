d9cd79552cc0c3a0dec90740bb20d731b6fbc3e5
Assylzhan91 <asylzhan.baimuratov@gmail.com>
2020-04-13 12:53:26 +0600

    ready to be developing project and lerning

--- src/App.js
+++ src/App.js
@@ -1,83 +1,14 @@
-import React, {useState, useEffect, lazy, Suspense} from 'react';
-import TodoList from './Todo';
-import Preloader from './Preloader/';
-import Modal from './Modal';
-import { Context } from './context';
-import AddTodo from './AddTodo';
-import app from './App.module.css';
+import React, { Component } from 'react'
+import cls from "./App.module.css"
 
+class App extends Component {
 
+  render() {
+    return (
+        <div className={`container ${cls.wrapper}`}>
 
-function App() {
-
-    const [todosState, setTodos] = useState([])
-    const [preloader, setPreloader] = useState(true)
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/todos?_limit=4')
-            .then(response => response.json())
-            .then(todos => {
-                setTimeout(()=>{
-                    setTodos(todos)
-                    setPreloader(false)
-                }, 1000)
-             })
-
-    }, [])
-
-    function checkboxHandler(idx){
-        setTodos(
-            todosState.map(todo =>{
-                if(todo.id === idx){
-                    todo.completed = !todo.completed
-                }
-                return todo
-            })
-        )
-     }
-
-    function removeTodoItem(idx){
-        setTodos(todosState.filter(({id})=>id !==idx))
-    }
-
-    function AddTodoItem(title){
-
-        setTodos(
-            todosState.concat([{
-                title: title.trim(),
-                completed: false,
-                id: Date.now()
-            }])
-        )
-    }
-
-  return (
-        <Context.Provider value={{removeTodoItem, AddTodoItem}}>
-            <div className={app.wrapper}>
-                <h1 className={app.mainTitle}>Todo list developed with hooks</h1>
-                <Modal/>
-
-                {
-                    preloader
-                    ?   <div className={app['wrapper-center']}><Preloader/></div>
-                    :   <AddTodo/>
-                 }
-                <div className={preloader?  `${app.center}` : null}>
-                {
-                    preloader ?
-                        <Preloader/>
-                        : todosState.length
-                            ?   <TodoList todos={todosState}
-                                    checkboxHandler={checkboxHandler}
-                                    removeTodoItem={removeTodoItem}
-                                />
-                            : <p>Not any todo, please add an todo</p>
-
-                }
-                </div>
-
-            </div>
-        </Context.Provider>
-  );
+        </div>
+    )
+  }
 }
-
-export default App;
+export  default App
